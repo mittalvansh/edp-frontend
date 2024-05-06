49e173fcebf7ee2564cd38d8663d5b603e4e8e19
Vansh Mittal <mittalvansh69@gmail.com>
2024-05-06 19:23:55 +0530

    fix diagnosis

--- src/components/diagnosisIntro.jsx
+++ src/components/diagnosisIntro.jsx
@@ -1,69 +1,60 @@
-import { Box, Grid, Stack, Group, Text, Image, Button, Flex } from "@mantine/core";
-import { useMediaQuery } from "@mantine/hooks";
-import { IconChevronLeft } from "@tabler/icons-react";
+import { Box, Flex, Grid, Stack, Text, Image, Button } from "@mantine/core";
 
 export default function DiagnosisIntro({ page, setPage }) {
   return (
-    <Box p="25px">
-      <Group align="center" gap={5}>
-        <IconChevronLeft size={36} />
-        <Text c="#1F4145" size="1.5rem" fw={600}>
-          Starting Diagnosis
-        </Text>
-      </Group>
+    <Box p="4rem">
+      <Text c="#1F4145" size="5rem" fw={600}>
+        Starting Diagnosis
+      </Text>
 
-      <Grid mt="1.5rem">
+      <Grid mt="3.5rem">
         <Grid.Col span={6}>
           <Stack gap="2rem">
             <Stack gap="1rem">
-              <Text c="#1F4145" size="1.25rem" fw={600}>
+              <Text c="#1F4145" size="2.5rem" fw={600}>
                 Diagnosis
               </Text>
-              <Text c="#1F4145" size="1rem">
-                We are starting with the diagnosis process. But first we need
-                to check your vitals
+              <Text c="#1F4145" size="2rem">
+                We are starting with the diagnosis process. But first we need to
+                check your vitals
               </Text>
             </Stack>
             <Stack gap="1rem">
-              <Text c="#089BAB" size="1.25rem" fw={600}>
+              <Text c="#089BAB" size="2.5rem" fw={600}>
                 Why?
               </Text>
-              <Text c="#1F4145" size="1rem">
+              <Text c="#1F4145" size="2rem">
                 Checking Vitals is important for us as this helps us make sure
                 that the diagnosis we provide are more accurate.
               </Text>
             </Stack>
-            <Text c="#1F4145" size="1rem" fw={300}>
+            <Text c="#1F4145" size="2rem" fw={400}>
               Note: We don't store any biometric information. This step only
               collects vital data important for better diagnosis.
             </Text>
           </Stack>
         </Grid.Col>
-        <Grid.Col span={6} align='center'>
-          <Text c='#667085' size='12px' mb='10px' style={{ lineHeight: '20px' }}>Please keep your finger on the corresponding sensor that glows up. </Text>
-          <Image
-            src='/Fingerprint-bro.svg'
-            alt=""
-            h={"252px"}
-            w={"272px"}
-            mb='15px'
-          />
-          <Flex align='flex-end' justify='flex-end'>
-            <Button
-              w="172px"
-              h="48px"
-              radius="xl"
-              bg="#078871"
-              mt="20px"
-              onClick={() => { setPage(1); }}
-            >
-              <Text size="16px" fw={600}>
-                Proceed
-              </Text>
-            </Button>
-          </Flex>
+        <Grid.Col span={6} align="center">
+          <Text c="#667085" size="2rem" mb="1rem" ta="right">
+            Please keep your finger on the corresponding sensor that glows up.{" "}
+          </Text>
+          <Image src="/fingerprint.svg" alt="" h="400px" w="400px" mb="15px" />
         </Grid.Col>
       </Grid>
+
+      <Flex w="100%" justify="flex-end" align="flex-end">
+        <Button
+          radius="xl"
+          size="xl"
+          bg="#078871"
+          mt="20px"
+          onClick={() => {
+            setPage(1);
+          }}
+        >
+          Proceed
+        </Button>
+      </Flex>
     </Box>
   );
 }
